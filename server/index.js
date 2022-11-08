eaf157719008a67523330f9950ca90479f4f8f3f
alaksandar_km <ebenzomarket@gmail.com>
2022-11-08 02:01:59 +0100

    start backend: express, sequuelize,  postgres db, models

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-"!"
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log("Server starts on PORT " + PORT))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
