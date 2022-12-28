37472ea724555be8a641c63064ddec0d486b278f
alaksandar_km <ebenzomarket@gmail.com>
2022-12-28 17:47:52 +0100

    shop page: TypeBar, BrandBar

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -4,18 +4,34 @@ export default class DeviceStore {
     constructor() {
         this._types = [
             {id: 1, name: 'Fridges'},
-            {id: 1, name: 'Smartphones'},
+            {id: 2, name: 'Smartphones'},
+            {id: 3, name: 'Headphones'},
+            {id: 4, name: 'Laptops'},
         ]
         this._brands = [
             {id: 1, name: 'Apple'},
-            {id: 1, name: 'Samsung'},
+            {id: 2, name: 'Samsung'},
+            {id: 3, name: 'Lenowo'},
+            {id: 4, name: 'Xiaomi'},
+            {id: 5, name: 'Motorolla'}
         ]
         this._devices = [
             {id: 1, name: "iPhone 12 mini 256GB", price: 4000, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
             {id: 2, name: "iPhone 12 mini 128GB", price: 3500, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
             {id: 3, name: "iPhone 12 mini 64GB", price: 3000, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
             {id: 4, name: "iPhone 12 mini", price: 2500, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
+            {id: 5, name: "iPhone 12v mini 256GB", price: 4000, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
+            {id: 6, name: "iPhone 12b mini 128GB", price: 3500, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
+            {id: 7, name: "iPhone 12 nmini 64GB", price: 3000, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
+            {id: 8, name: "iPhone 12m mini", price: 2500, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
+            {id: 9, name: "iPhone 12, mini 256GB", price: 4000, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
+            {id: 10, name: "iPhone 12j mini 128GB", price: 3500, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
+            {id: 11, name: "iPhone 12h mini 64GB", price: 3000, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
+            {id: 12, name: "iPhone 12t mini", price: 2500, rating: 5, img: "https://www.telepolis.pl/images/katalog/Apple/Apple_iPhone_12_mini/apple-iphone-12-mini-5.jpg"},
         ]
+
+        this._selectedType = {}
+        this._selectedBrand = {}
 
         makeAutoObservable(this)
     }
@@ -33,6 +49,14 @@ export default class DeviceStore {
         this._devices = devices
     }
 
+    setSelectedType(type) {
+        this._selectedType = type
+    }
+
+    setSelectedBrand(brand) {
+        this._selectedBrand = brand
+    }
+
     // computed fns: get vars from state, is called if var changes
     get types() {
         return this._types
@@ -44,5 +68,13 @@ export default class DeviceStore {
 
     get devices() {
         return this._devices
+    }
+
+    get selectedType() {
+        return this._selectedType
+    }
+
+    get selectedBrand() {
+        return this._selectedBrand
     }
 }
