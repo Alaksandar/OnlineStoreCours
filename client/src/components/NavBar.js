be47d0b4b5fac1b17617bd18e42b0d606faf6d9f
alaksandar_km <ebenzomarket@gmail.com>
2022-12-28 16:19:21 +0100

    start login & registration

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,7 @@
 import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
-
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import Button from 'react-bootstrap/Button'
-
+import { Container, Nav, Navbar, Button } from 'react-bootstrap'
 import { Context } from '..'
 import { SHOP_ROUTE } from '../utils/constants'
 
